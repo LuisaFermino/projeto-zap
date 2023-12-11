a8655e76fec9d62158ecebde850188c695132eb0
LuisaFermino <souzafermino49@gmail.com>
2023-12-11 14:42:49 -0300

    Tela inicial finalizada

--- src/TelaInicial.js
+++ src/TelaInicial.js
@@ -3,7 +3,10 @@ import "./App.css";
 export default function TelaInicial() {
   return (
     <div className="tela-inicial">
-      <img src="../img/logo.png" alt="logo" className="logo-grande" />
+      <div className="container-logo">
+        <img src="../img/logo.png" alt="logo" className="logo-grande" />
+      </div>
+
       <p className="titulo">ZapRecall</p>
       <button className="botao-iniciar">Iniciar Recall!</button>
     </div>
